d9726a6ac8958967ed043ed0dfbdab4be0fe5f60
Raghu <Nagamraghavendra8@gmail.com>
2023-08-25 12:49:20 +0530

    Adding fibo series

---
+++ fabinocci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int a=0,b=1,c,n;
+    cin>>n;
+    vector<int>v;
+    v.push_back(a);
+    v.push_back(b);
+    for(int i=0;i<n-2;i++){
+        c=a+b;
+        v.push_back(c);
+        a=b;b=c;
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i];
+        return 0;
+    }
+}
